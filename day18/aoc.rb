9012b69836d321dc820ceeae181c8ed2a0c32edb
Etienne Ischer <etienne.ischer@gmail.com>
2023-12-18 09:55:09 +0100

    day18 par 2

--- day18/aoc.rb
+++ day18/aoc.rb
@@ -6,7 +6,7 @@ class Hole
   def initialize(params = {})
     @y = params.fetch(:y)
     @x = params.fetch(:x)
-    @rgb = params.fetch(:rgb)
+    @rgb = params.fetch(:rgb, '#000000')
   end
 end
 
@@ -81,4 +81,70 @@ puts "Area: #{area}"
 puts "Perimeter: #{perimeter}"
 
 # Part 1: 40745
+puts "Real area = #{area + (perimeter / 2) + 1}"
+
+# Part 2
+
+# dig_plan = []
+vertices = []
+
+x = 0
+y = 0
+
+file_data.each do |line|
+  line = line.split[2].gsub(/(#|\(|\))/, '')
+  distance = line[0..4].hex
+  case line[5]
+  when '0'
+    distance.times do
+      x += 1
+      # dig_plan << Hole.new(x:, y:, rgb: line[2])
+    end
+  when '1'
+    distance.times do
+      y += 1
+      # dig_plan << Hole.new(x:, y:, rgb: line[2])
+    end
+  when '2'
+    distance.times do
+      x -= 1
+      # dig_plan << Hole.new(x:, y:, rgb: line[2])
+    end
+  when '3'
+    distance.times do
+      y -= 1
+      # dig_plan << Hole.new(x:, y:, rgb: line[2])
+    end
+  end
+
+  vertices << Hole.new(x:, y:)
+end
+
+sum_vertices = 0
+perimeter = 0
+
+# area of a irregular polygon
+# A = 0.5 * |(x1*y2 - x2*y1) + (x2*y3 - x3*y2) + ... + (xn*y1 - x1*yn)|
+vertices.each_with_index do |vertice, index|
+  # puts "Vertice y=#{vertice.y} x=#{vertice.x}"
+  sum_vertices += if index < vertices.length - 1
+                    (vertice.x * vertices[index + 1].y) - (vertices[index + 1].x * vertice.y)
+                  else
+                    (vertice.x * vertices[0].y) - (vertices[0].x * vertice.y)
+                  end
+
+  perimeter += if index < vertices.length - 1
+                 (vertices[index + 1].x - vertice.x).abs + (vertices[index + 1].y - vertice.y).abs
+               else
+                 (vertices[0].x - vertice.x).abs + (vertices[0].y - vertice.y).abs
+               end
+
+  # puts "sum vertices : #{sum_vertices}"
+end
+
+area = 0.5 * sum_vertices.abs
+puts "Area: #{area}"
+puts "Perimeter: #{perimeter}"
+
+# Part 2
 puts "Real area = #{area + (perimeter / 2) + 1}"
